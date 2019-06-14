d54061c4a3f7983d23f8358b8a8410d840c0c46c
Vianney Gall <41265342+IterateMe@users.noreply.github.com>
2019-06-14 08:35:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import time
 
 import RPi.GPIO as GPIO
-import API as HO
+import API
 
-water_valve = HO.EValve(16,15,12)
-on_led = HO.Led(13)
-off_button = HO.Button(11, 13)
+water_valve = API.EValve(16,15,12)
+on_led = API.Led(13)
+off_button = API.Button(11, 13)
 
 off_button.set_detection(off_button.close_RPi)
 
